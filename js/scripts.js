c8676afa4202e8444e98f69f6c15bc9f5f371643
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-23 13:15:18 +0300

    Integrate the files to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 
 function toggleMenu() {
+    document.getElementById('toggle-menu').classList.toggle('hidden');
     document.getElementById('toggle-menu').classList.toggle('toggle-menu');
-    document.querySelector('header').classList.toggle('remove-padding');
+    document.getElementById('desktop-nav').classList.toggle('hidden');
+    document.getElementById('toggle-btn').classList.toggle('hidden');
+    document.getElementById('logo').classList.toggle('hidden');
+
+
+
+
+    // document.querySelector('header').classList.toggle('remove-padding');
   }
-  function mobileMenuOption(event, id) {
+
+function mobileMenuOption(event, id) {
     event.preventDefault();
     toggleMenu();
     const linkTo = document.getElementById(id);
     linkTo.scrollIntoView();
-  }
+}
