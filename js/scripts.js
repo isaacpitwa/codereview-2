3fd281a184073bc6293d3cd4b5aefbdba89a1a24
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-24 11:03:00 +0300

    Fix Humberh button

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
-
 function toggleMenu() {
-    document.getElementById('toggle-menu').classList.toggle('hidden');
-    document.getElementById('toggle-menu').classList.toggle('toggle-menu');
-    document.getElementById('desktop-nav').classList.toggle('hidden');
-    document.getElementById('toggle-btn').classList.toggle('hidden');
-    document.getElementById('logo').classList.toggle('hidden');
-
-    // document.querySelector('header').classList.toggle('remove-padding');
-  }
+  document.getElementById('toggle-menu').classList.toggle('hidden');
+  document.getElementById('toggle-menu').classList.toggle('toggle-menu');
+  document.getElementById('desktop-nav').classList.toggle('hidden');
+  document.getElementById('toggle-btn').classList.toggle('hidden');
+  document.getElementById('logo').classList.toggle('hidden');
+}
 
 function mobileMenuOption(event, id) {
-    event.preventDefault();
-    toggleMenu();
-    const linkTo = document.getElementById(id);
-    linkTo.scrollIntoView();
+  event.preventDefault();
+  toggleMenu();
+  const linkTo = document.getElementById(id);
+  linkTo.scrollIntoView();
 }
